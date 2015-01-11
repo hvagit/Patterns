5026ae2d8999ea433bb6f3e473e2fc4225678b39
hvagit <henrik1969@gmail.com>
2015-01-11 18:03:48 +0100

    Init

---
+++ src/composite/Graphic.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composite;
+
+/**
+ *
+ * @author Henrik
+ */
+/** "Component" */
+public interface Graphic {
+       //Prints the graphic.
+    public void print();
+}
